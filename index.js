d684d582f5b8c597162ce541af55dee00fb33447
Nate Hunter <hunter.j.nate@gmail.com>
2018-12-03 15:20:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var beatleArray = [];
+  for (let i = 0; i < musicians.length; i++) {
+    beatleArray.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return beatleArray;
+}
+
+function johnLennonFacts(facts) {
+  while 
+}
